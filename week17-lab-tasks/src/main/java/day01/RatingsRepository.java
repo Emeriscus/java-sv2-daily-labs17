f4f9b1cf4407962c712f8ca4462c4727e06fffba
Emeriscus <kimre88@gmail.com>
2022-02-24 15:33:01 +0100

    update

--- week17-lab-tasks/src/main/java/day01/RatingsRepository.java
+++ week17-lab-tasks/src/main/java/day01/RatingsRepository.java
@@ -44,36 +44,58 @@ public class RatingsRepository {
         }
     }
 
-    public void insertAverageRatingToMovies(long movieID, double rating) throws SQLException {
+    public void insertAverageRatingToMovies(long movieID, double rating) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement stmt = conn.prepareStatement("UPDATE movies SET avg_rating = (?) where id = (?);")) {
             stmt.setDouble(1, rating);
             stmt.setLong(2, movieID);
             stmt.executeUpdate();
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Cannot insert", sqle);
         }
     }
+
+// Kristóf megoldás:
 
     private double getMovieAverageRating(long movieId) throws SQLException {
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement("select * from ratings where movie_id = ?")) {
+             PreparedStatement stmt = conn.prepareStatement
+                     ("select avg(rating) as calculated_avg from ratings join movies on movies.id=ratings.movie_id where movies.id = ?")) {
             stmt.setLong(1, movieId);
-            return getAverageRating(processStatement(stmt));
-        }
-    }
-
-    private List<Double> processStatement(PreparedStatement stmt) throws SQLException {
-        List<Double> ratings = new ArrayList<>();
-        try (ResultSet rs = stmt.executeQuery()) {
-            while (rs.next()) {
-                ratings.add(rs.getDouble("rating"));
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getDouble("calculated_avg");
+                }
             }
+            throw new IllegalArgumentException("Cannot find movie");
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Cannot query", sqle);
         }
-        return ratings;
     }
 
-    private Double getAverageRating(List<Double> ratings) {
-        double sum = ratings.stream().mapToInt(Double::intValue).sum();
-        long count = ratings.stream().count();
-        return sum / count;
-    }
+// Saját megoldás:
+
+//    private double getMovieAverageRating(long movieId) throws SQLException {
+//        try (Connection conn = dataSource.getConnection();
+//             PreparedStatement stmt = conn.prepareStatement("select * from ratings where movie_id = ?")) {
+//            stmt.setLong(1, movieId);
+//            return getAverageRating(processStatement(stmt));
+//        }
+//    }
+//
+//    private List<Double> processStatement(PreparedStatement stmt) throws SQLException {
+//        List<Double> ratings = new ArrayList<>();
+//        try (ResultSet rs = stmt.executeQuery()) {
+//            while (rs.next()) {
+//                ratings.add(rs.getDouble("rating"));
+//            }
+//        }
+//        return ratings;
+//    }
+//
+//    private Double getAverageRating(List<Double> ratings) {
+//        double sum = ratings.stream().mapToInt(Double::intValue).sum();
+//        long count = ratings.stream().count();
+//        return sum / count;
+//    }
 }
